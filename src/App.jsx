128b2ce2e6ec31c818758288ed99bc4374b692ce
abdelmonemanwr <abdelmonemanwr7777@gmail.com>
2024-06-10 12:47:58 +0300

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { MyNavbar } from './components/MyNavbar';
-import { MyFooter } from './components/MyFooter';
-import { MySlider } from './components/MySlider';
-import { Products } from './components/Products/Products';
 
 export default function App() {
     return (
-        <div className="App">
-            < MyNavbar />
-            < MySlider />
-            < Products />
-            < MyFooter />
-        </div>
+        
     );
 }
